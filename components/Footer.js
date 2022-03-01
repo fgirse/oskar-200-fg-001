1671457b4c0dbdca6a08a9410d4df054bfda3901
fgirse <fgirse@bluewin.ch>
2022-03-01 17:15:39 +0100

    commit 9.

--- components/Footer.js
+++ components/Footer.js
@@ -9,14 +9,22 @@ export default function Footer() {
   return (
     <footer className="bg-red-900 w-full border-yellow-300mx-auto">
 
-          <section className='md:flex md:flex-row md:justify-Elderly md:items-center'>
+          <section className='md:p-3 md:mt-5 md:flex md:flex-row md:justify-center md:items-baseline'>
             {/*======================================== Flexbox ================================================ */}
             {/*======================================== Item 1  ================================================ */}
-            <div className='hidden md:block md:w-4/12 md:h-36'></div>
-
+           
+            <div className='flex flex-col w-4/12 justify-center items-center gap-x-5 md:flex md:items-ceenter first-letter:first-line:          md:gap-x-0x md:justify'>
+            <p className='text-white'>Partner:</p>
+            <div className="flex flex-row gap-8 justify-elderly">
+            <LogoLeckerLaden className="w-28 h-9"></LogoLeckerLaden>
+            <Astra className="w-12 h-8"/>
+            <Ganter className="w-12 h-9"/>
+            </div>
+            
+          </div>
 
             {/*======================================== Item 2  ================================================ */}
-            <div className="w-4/12 flex flex-col items-center mt-16">
+            <div className="w-4/12 flex flex-col items-center">
                   <div className="flex mb-3 space-x-4 ">
                     <SocialIcon
                       kind="mail"
@@ -29,7 +37,7 @@ export default function Footer() {
                     <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
                     <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
                   </div>
-                    <div className="mx-autoaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaex flex-row items-center justify-center mb-2 space-x-2 text-xs text-gray-300 dark:text-gray-400 font-mono ">
+                    <div className="mx-auto flex-row items-center justify-center mb-2 space-x-2 text-xs text-gray-300 dark:text-gray-400 font-mono ">
                       <div className="mx-auto flex flex-row lg:text-sm justify-center gap-x-2">
                              {/*<div>{siteMetadata.author}</div>
                              <div>{` • `}</div>*/}
@@ -50,19 +58,11 @@ export default function Footer() {
             </div>
       {/*======================================== Item 3  ================================================ */}
             
-
-          <div className='flex flex-col w-4/12 justify-start items-end gap-x-5 md:mt-12 md:flex md:items-start                                             md:gap-x-0x md:justify'>
-            <p className='text-white'>Partner:</p>
-            <div className="flex flex-row gap-8 justify-elderly">
-            <LogoLeckerLaden className="w-28 h-9"></LogoLeckerLaden>
-            <Astra className="w-12 h-8"/>
-            <Ganter className="w-12 h-8"/>
-            </div>
-            
-          </div>
-            
+ 
+          
+       {/*======================================== Flexbox Ende ================================================ */}      
           </section>
-      {/*======================================== Flexbox Ende ================================================ */}
+     
             
 
     </footer>
